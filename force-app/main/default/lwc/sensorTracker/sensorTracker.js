a7ad9d1f8cbe3042776434aeffae4e6ae6f78601
Your Name <you@example.com>
2024-07-18 18:17:38 +0300

    added paginator, test classes, change components

--- force-app/main/default/lwc/sensorTracker/sensorTracker.js
+++ force-app/main/default/lwc/sensorTracker/sensorTracker.js
@@ -1,24 +1,51 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getSensorList from '@salesforce/apex/sensorController.getSensorList';
+import getSensorEventTileList from '@salesforce/apex/sensorController.getSensorEventTileList';
 
 export default class SensorTracker extends LightningElement {
     selectedSensor;
-    hasSensorEvents;
-    sensorEvents;
+    @track hasSensorEvents;
+    @track sensorEvents;
+    sensorId;
+    pageNumber = 1;
     @wire(getSensorList)
     sensors;
+    @wire(getSensorEventTileList, { sensorId: '$sensorId', pageNumber: '$pageNumber' })
+    wiredSensorEvents({ error, data }) {
+        if (data) {
+            this.sensorEvents = data.records;
+            if (this.selectedSensor) {
+                this.hasSensorEvents = true;
+            }
+        } else if (error) {
+            this.sensorEvents = undefined;
+            this.hasSensorEvents = false;
+            console.error('Error fetching sensor events: ', error);
+        }
+    }
 
 
 
     handleSelect(event) {
-        const sensorId = event.detail;
-        this.selectedSensor = this.sensors.data.find(
-            (sensor) => sensor.Id === sensorId
-        );
+        this.sensorId = event.detail;
+        if (this.sensors && this.sensors.data) {
+            this.selectedSensor = this.sensors.data.find(
+                (sensor) => sensor.Id === this.sensorId
+            );
+        }
 
-        if (this.selectedSensor && this.selectedSensor.Sensor_Events__r) {
-            this.hasSensorEvents = this.selectedSensor.Sensor_Events__r;
+        if (this.selectedSensor && this.sensorEvents) {
+            this.hasSensorEvents = true;
+        } else {
+            this.hasSensorEvents = false;
         } 
-        console.log('events ---- '+ this.selectedSensor.Sensor_Events__r); 
+    }
+
+    handlePreviousPage() {
+        this.pageNumber = this.pageNumber - 1;
+    }
+
+    handleNextPage() {
+        this.pageNumber = this.pageNumber + 1;
     }
 }
